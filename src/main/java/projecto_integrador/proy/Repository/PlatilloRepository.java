4461c3e0c13f7ab66f685fe345fbed3400a7bad1
Andre Solis Palacios <138182291+KaidennS@users.noreply.github.com>
2024-10-24 16:14:49 -0500

    Delete src/main directory

--- src/main/java/projecto_integrador/proy/Repository/PlatilloRepository.java
+++
@@ -1,8 +0,0 @@
-package projecto_integrador.proy.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projecto_integrador.proy.Model.Platillo;
-
-public interface PlatilloRepository extends JpaRepository<Platillo, Long> {
-    // Puedes agregar métodos personalizados aquí si los necesitas
-}
--- src/main/java/projecto_integrador/proy/Repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package projecto_integrador.proy.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projecto_integrador.proy.Model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Usuario findByCorreo(String correo);
-}
